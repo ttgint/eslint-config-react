89fe449e3cdea9f0e47aebaf559f2c1228edb864
cemremengu <cemremengu@gmail.com>
2019-07-22 15:59:33 +0300

    Change config

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports = {
-  parser: 'babel-eslint',
-  extends: ['airbnb', 'react-app', 'plugin:prettier/recommended'],
+  parser: "babel-eslint",
+  extends: ["airbnb", "react-app", "prettier", "prettier/react"],
   rules: {
-    'no-console': 'warn',
-    'jsx-a11y/href-no-hash': [0],
-    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
-    'react/jsx-wrap-multilines': 'off',
-    'react/no-did-update-set-state': 'off',
-    'react/destructuring-assignment': 'off'
+    "no-console": "warn",
+    "jsx-a11y/href-no-hash": [0],
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "react/jsx-wrap-multilines": "off",
+    "react/no-did-update-set-state": "off",
+    "react/destructuring-assignment": "off"
   }
-}
+};
